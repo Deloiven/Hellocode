a52107b97fb3c5438b96bf567af90fa8348fd250
Your <vlasuyk.2015.1@mail.ru>
2022-07-08 17:22:47 +0300

    add Exempale005_ConditionIfelse

---
+++ Exempale005_ConditionIfelse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "валера")
+{
+    Console.WriteLine("Ура, это же Валера!!!");
+
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
